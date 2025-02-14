3db294cfc87f8611ba015fd7528d94a5ae938e1f
Nicolás Ortega Froysa <nicolas@ortegas.org>
2025-02-14 14:30:56 +0100

    Switch global config to PHP.

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+# last updated the site.
+$siteupdated = "2025-01-25";
+
+# site title (part of page ${title} probably).
+$sitetitle = "The Music in Noise";
+# prefix site url.
+$siteurl = "https://themusicinnoise.net";
+# site mail used for contact "mail link".
+$sitemail = "nicolas AT ortegas DOT org";
+
+## page
+# page language.
+$lang = "en-US";
+# author (global default).
+$author = "Nicolás Ortega Froysa";
+# site keywords (global default), don't use too many.
+$keywords = "programming, language, society, religion, blog, catholicism";
+# site description (global default).
+$description = "Personal website for publishing projects and blog posts.";
+# copyright year
+$copyyear = "2025";
+
+# default title.
+$title = "";
+?>
