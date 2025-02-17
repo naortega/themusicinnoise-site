bd816e74fbc69959e5b8fe52944d11e0d377c831
Nicolás Ortega Froysa <nicolas@ortegas.org>
2025-02-17 14:26:58 +0100

    Add RSS file.

---
+++ pages/blog/rss.xml.php
@@ -0,0 +1,32 @@
+<?php
+require 'config.php';
+?>
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0"
+  xmlns:content="http://purl.org/rss/1.0/modules/content/"
+  xmlns:dc="http://purl.org/dc/elements/1.1/">
+	<channel>
+		<title><?= $sitetitle ?></title>
+		<description><?= $description ?></description>
+		<link><?= $siteurl ?></link>
+<?php
+$dir_files = scandir(dirname(__FILE__), SCANDIR_SORT_DESCENDING);
+foreach($dir_files as $file_info) {
+	if(!str_ends_with($file_info, ".cfg.php"))
+		continue;
+
+	require dirname(__FILE__) . "/" . $file_info;
+?>
+		<item>
+			<title><?= $title ?></title>
+			<link><?= $siteurl ?>/blog/<?= $file_info ?></link>
+			<guid><?= $siteurl ?>/blog/<?= $file_info ?></guid>
+			<dc:date><?= $created ?>T00:00:00Z</dc:date>
+			<author><?= $author ?></author>
+			<description><?= $description ?></description>
+		</item>
+<?php
+}
+?>
+	</channel>
+</rss>
