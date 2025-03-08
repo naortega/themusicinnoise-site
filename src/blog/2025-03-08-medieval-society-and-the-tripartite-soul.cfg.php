ba5552c5d07b08831e826dab8772baa7a05b6cd0
Nicolás Ortega Froysa <nicolas@ortegas.org>
2025-03-08 18:32:02 +0100

    Blog: Add "Medieval Society and the Tripartite Soul"

---
+++ src/blog/2025-03-08-medieval-society-and-the-tripartite-soul.cfg.php
@@ -0,0 +1,6 @@
+<?php
+$title = "Medieval Society and the Tripartite Soul";
+$description = "Understanding Medieval Society as the implementation of Plato's ideal.";
+$created = "2025-03-08";
+$updated = "2025-03-08";
+?>
---
+++ src/blog/2025-03-08-medieval-society-and-the-tripartite-soul.html.php
@@ -0,0 +1,62 @@
+<?php
+require 'config.php';
+require '2025-03-08-medieval-society-and-the-tripartite-soul.cfg.php';
+require 'templates/blog-header.php';
+?>
+
+<p>
+In Plato's <i>Republic</i> he argues that the soul is composed of three parts:
+the logical, the spirited, and the appetitive. The logical (or rational) part is
+that which cares for and seeks the truth. The spirited that part by which we are
+impassioned by what we could (for the sake of simplicity) call emotions, such as
+anger or joy. The appetitive that which is concerned with matters of material
+appetite, such as food, sex, and other such pleasures. He also argues that these
+three parts, if the soul is to be well-ordered, ought to be placed in a
+hierarchy where the logical rules over the spirited and the appetitive, and the
+spirited is subject to the logical and commands the appetite.
+</p>
+
+<p>So far, all of this should seem to make sense, especially to a Catholic who
+understands that we first seek God (i.e. Truth) and subject our whole being to
+Him; we then rule over our passions, not to quench them, but to direct them to
+that which is conducive to God's glory, which often involves (especially now
+during the penitential season of Lent) the control of the appetite. But Plato
+goes on to say that what can be found in the soul may also be found in the city
+(i.e. society): that it too has these three parts and ought be ordered so. The
+argument I would like to make is that Medieval Society (i.e. Christendom)
+represented this order. With its imperfections, certainly, but nonetheless I
+think it the case that Medieval Society functioned under this model.
+</p>
+
+<p>
+It is generally well known that the Medieval Society was divided into three main
+parts: the Church, the rulers, and the workers. Of these three it seems quite
+evident how they line up with the parts of the soul. The Church represents the
+logical, not because it is primarily made up of philosophers, but because its
+primary concern is with the Truth, i.e. God who is the highest truth. The rulers
+are the spirited, as being both warriors and rulers made them seekers of glory,
+but namely they were the only ones capable of establishing order based on higher
+ideals. The workers were those concerned with the more mundane and merely
+necessary affairs of food and crafts. My argument is thus that in the Middle
+Ages society was organized such that the Church ruled of the rulers and the
+workers, and the rulers subjected themselves to the Church and commanded the
+workers.
+</p>
+
+<p>
+To modern audiences this may be strange considering that the highest authority
+within a kingdom was considered to be its king, but this is because in today's
+secular society we do not understand how the Church could influence society (and
+indeed its governance) without necessarily forming a part of the governing body
+(i.e. what we now call the State). Medieval Society took religion seriously,
+thus understanding that God's representatives on Earth carried real authority
+thought ought not be disregarded, especially as it pertains to matters of faith
+and morals. If not for this it would have been impossible for popes to have
+convoked the entirety of Christendom to fight in the Crusades. Thus it was not
+necessary that the highest authority in a kingdom be a cleric, for all kings
+recognized the highest authority on Earth to be none other than God himself.
+</p>
+
+<?php
+require 'templates/blog-footer.php';
+?>
