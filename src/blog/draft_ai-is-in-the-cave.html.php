66b7951ee21f42134547ae4b8fa688f0a3ec364e
Nicolás Ortega Froysa <nicolas@ortegas.org>
2025-03-13 09:58:30 +0100

    Blog: Add draft for AI article.

---
+++ src/blog/draft_ai-is-in-the-cave.html.php
@@ -0,0 +1,18 @@
+<?php
+require 'config.php';
+require 'draft_ai-is-in-the-cave.cfg.php';
+require 'templates/blog-header.php';
+?>
+<p>
+In spite of being a software developer and knowing how much of modern digital
+technologies actually work under-the-hood, I'm also aware that for most people a
+lot of this can seem like magic. Somehow a bunch of ones and zeros can be
+transformed into a movie which is streamed over cables and even thin-air until
+it reaches my tablet and is transformed to something actually recognizable on
+the screen. Luckily, after years of having to explain to my elderly grandparents
+how many of these technologies work, I've been blessed with an ability to make
+up analogies which make these things more understandable.
+</p>
+<?php
+require 'templates/blog-footer.php';
+?>
