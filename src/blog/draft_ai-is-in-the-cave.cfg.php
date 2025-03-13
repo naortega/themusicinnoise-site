e79dbfeb912dbd300f79a5c6bee3ec22124549b0
Nicolás Ortega Froysa <nicolas@ortegas.org>
2025-03-13 13:35:54 +0100

    Blog: Add more to AI draft.

--- src/blog/draft_ai-is-in-the-cave.cfg.php
+++ src/blog/draft_ai-is-in-the-cave.cfg.php
@@ -1,6 +1,6 @@
 <?php
 $title = "AI Is in the Cave";
-$description = "Delving into the (apparent) mystery of Large Language Models (a.k.a. A.I.) and why none of them are actually intelligent.";
+$description = "Delving into the (apparent) mystery of Large Language Models (a.k.a. AI) and why none of them are actually intelligent.";
 $created = "2025-03-13";
 $updated = "2025-03-13";
 ?>
--- src/blog/draft_ai-is-in-the-cave.html.php
+++ src/blog/draft_ai-is-in-the-cave.html.php
@@ -13,6 +13,38 @@ the screen. Luckily, after years of having to explain to my elderly grandparents
 how many of these technologies work, I've been blessed with an ability to make
 up analogies which make these things more understandable.
 </p>
+
+<p>
+When it comes to Large Language Models (LLMs, a.k.a. AI) the usual amazement and
+mystification of technology reached new peaks as folks started speculating that
+perhaps this is truly the point where computers can pass the Turing
+Test,<sup><a href="#r1" >[1]</a></sup> and the more enthusiastic among these
+would go further and say that perhaps even the machines could gain
+conciousness<sup><a href="#n1" >(1)</a></sup> if they haven't already, all
+ultimately raising the question of whether we've reached the point of
+Singularity.<sup><a href="#r2" >[2]</a></sup>
+</p>
+
+<h2>The Man Who "Learned" Chinese</h2>
+
+<h2>Here be Demons</h2>
+
+<h2>Resources</h2>
+<h3>Notes</h3>
+<ol class="notes" >
+	<li id="n1" >
+		Some people confuse the Turing Test to be an indicator that a machine
+		<em>has</em> reached conciousness, but this is a misunderstanding. The
+		test merely indicates that in a blind-folded scenario a human cannot
+		tell whether they are talking to another human or a machine, usually via
+		a text prompt.
+	</li>
+</ol>
+<h3>References</h3>
+<ol class="refs" >
+	<li id="r1" ><a href="https://en.wikipedia.org/wiki/Turing_test" target="_blank" >Turing test - Wikipedia</a></li>
+	<li id="r2" ><a href="https://en.wikipedia.org/wiki/Technological_singularity" target="_blank" >Technological singularity - Wikipedia</a></li>
+</ol>
 <?php
 require 'templates/blog-footer.php';
 ?>
