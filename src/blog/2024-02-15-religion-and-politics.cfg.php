e430c3d24b996ad32cf2fa8b9652a78a0b1fa9cb
Nicolás Ortega Froysa <nicolas@ortegas.org>
2025-05-16 19:39:11 +0200

    Blog: Use &amp;

--- src/blog/2024-02-15-religion-and-politics.cfg.php
+++ src/blog/2024-02-15-religion-and-politics.cfg.php
@@ -1,5 +1,5 @@
 <?php
-$title = "Religion & Politics";
+$title = "Religion &amp; Politics";
 $description = "An essay on the relationship between religion & politics, and how one's religious values may be legitimate in political conversation.";
 $created = "2024-02-15";
 $updated = "2024-02-15";
