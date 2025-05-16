a6a0f815f3c0db77eb022b340800eb255c5fb1ab
Nicolás Ortega Froysa <nicolas@ortegas.org>
2025-05-16 19:41:10 +0200

    Replace & with 'and'.

--- src/blog/2024-02-15-religion-and-politics.cfg.php
+++ src/blog/2024-02-15-religion-and-politics.cfg.php
@@ -1,6 +1,6 @@
 <?php
 $title = "Religion &amp; Politics";
-$description = "An essay on the relationship between religion & politics, and how one's religious values may be legitimate in political conversation.";
+$description = "An essay on the relationship between religion and politics, and how one's religious values may be legitimate in political conversation.";
 $created = "2024-02-15";
 $updated = "2024-02-15";
 ?>
