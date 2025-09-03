8f5516e915f0936e75ad322d3f174823305021f1
Nicolás Ortega Froysa <nicolas@ortegas.org>
2025-09-03 21:13:40 +0200

    Add "Is there Humor in Heaven?" post.

---
+++ src/blog/2025-09-03-is-there-humor-in-heaven.cfg.php
@@ -0,0 +1,6 @@
+<?php
+$title = "Is there Humor in Heaven?";
+$description = "A personal exercise in theological reasoning which I could totally be wrong about.";
+$created = "2025-09-03";
+$updated = "2025-09-03";
+?>
---
+++ src/blog/2025-09-03-is-there-humor-in-heaven.html.php
@@ -0,0 +1,180 @@
+<?php
+require 'config.php';
+require '2025-09-03-is-there-humor-in-heaven.cfg.php';
+require 'templates/blog-header.php';
+?>
+
+<p>
+Probably around a year ago I was having a conversation with a friend of mine
+over a beer (as you do) and at some point we got to talking about humor and what
+is and is not acceptable, particularly as it pertains to why blasphemous humor
+is wrong. The conclusion we reached was that the object of humor must be
+something imperfect, but God and the sacred are perfect, thus they aren't a
+proper object for humor. Then, this having been said, and as tends to happen, it
+sounded about right to both of us and we moved on. But later, on my way home,
+and for months ever since I have thought about not only whether this is true,
+but also what the implications of it may be.
+</p>
+
+<p>
+To start, I'd like to justify the claim before going any further, for otherwise
+we're really just speculating based on possibly flawed assumptions. Furthermore,
+I think the claim also requires some clarification, since without nuance it can
+probably lead to some wrong conclusions about what we should be laughing at.
+Therefore I'll start with the inductive case and then move on to the case based
+on what I think is the purpose of humor. After that we can get into what the
+consequences of this claim are.
+</p>
+
+<p>
+When it comes to humor we can simplify and say that it always manifests itself
+in some sort of a joke, or at least we'll use that terminology to refer to a
+specific instance of humor. Now when thinking of jokes, whether ironic,
+wordplay, sarcastic, etc. in all instances I have observed there is not a single
+joke which has as its object something perfect, but always something imperfect
+(or at least apparently imperfect). An imperfect expectation on the part of the
+listener, an imperfect (or ambiguous) communication of meaning in a pun, an
+imperfect use of language itself when one sarcastically says the opposite of
+what one means. The object of our humor is always the imperfection. Even when an
+atheist laughs at God himself, who is perfect being objectively, it is because
+his subjective understanding of God is itself imperfect, and it is this
+subjective understanding which is the object of his humor. I have dared friends
+and family to find me a single example to the contrary, but none have given me
+one example.
+</p>
+
+<p>
+We must then ask the question “why?” Why is it that this is the case? And for
+this we must tend to the purpose of humor and why we do it. For this I think
+there is wisdom to be found in the proverb “a cheerful heart is a good medicine,
+but a downcast spirit dries up the bones.” (Prov. 17:22) Or, as the modern
+version of this proverb goes: laughter is the best medicine. Laughter seems to
+be a means of healing; not the body, for which we have physicians, not the soul,
+for which we have the Sacraments and the priests that minister them, but the
+spirit. It takes that which is imperfect in life, and although it cannot fix it,
+it gives it meaning and, in the more tragic cases, it also makes the yoke a
+little bit lighter. In the case of the pun it makes the imperfect ambiguities of
+language attain the higher purpose of elevating the spirit, and in the case of a
+real tragedy, such as living under an oppressive regime, it provides relief
+enough to keep pushing onward. We thus see that humor attains an almost divine
+quality, as it does precisely what we Christians claim gives answer to the
+problem of evil: that “[i]n time we can discover that God in his almighty
+providence can bring a good from the consequences of an evil, even a moral evil,
+caused by his creatures” (CCC § 312). This is not to claim, of course, that
+humor makes up for the evil (which is what imperfection is) that is its object,
+but certainly that it is one small piece which God has readily put at our
+disposal.
+</p>
+
+<p>
+So now that we know this, we can finally ask the question: is there humor in
+Heaven? At first glance I think the answer must be a resounding “no.” The
+Catechism describes Heaven as “[the] perfect life with the Most Holy Trinity
+[...] the ultimate end and fulfillment of the deepest human longings, the state
+of supreme definitive happiness.” (CCC § 1024) If there is no imperfection,
+there is not object for humor. This may seem surprising, considering what joy we
+get from it, but there are other goods which likewise will not exist in Heaven
+for similar reasons. For what will we need forgiveness in Heaven if there are no
+transgressions? For what will we need medicine if our bodies will not need
+healing? Thus it is not outlandish to assume that humor too may fade as its
+function was only necessary on this side of the <i>Eschaton</i>. One could argue
+that perhaps we could continue to find humor in the imperfections of our past,
+but I find this unconvincing, for however long history may have been it will
+always be finite compared to the eternity we shall spend in Heaven, thus at some
+point we will have exhausted all the jokes and there will be no more new ones.
+So the eschatological future looks rather bleak for humor.
+</p>
+
+<p>
+There is, however, one possibility, though for some it may at first seem
+revolting, so bear with me. There is one place that for the rest of eternity
+will suffer imperfection and to enormous degrees: Hell and the suffering of the
+damned. Now certainly, to the damned their suffering would not seem at all
+humorous, nor can there be humor within Hell since God would not provide them
+with such a medicinal relief, for as the Catechism states:
+</p>
+
+<blockquote>
+	“The chief punishment of hell is eternal separation from God, in whom alone
+	man can possess the life and happiness for which he was created and for
+	which he longs.” (CCC § 1035)
+</blockquote>
+
+<p>
+This does not mean, however, that those in Heaven cannot witness and find humor
+in the imperfections of the suffering of the damned. This may seem cruel, but
+this I think will may sense with perhaps a bit of explanation and example.
+</p>
+
+<p>
+The reason we find it cruel is because we unfortunately imagine the damned to be
+unwilling in their damnation. This is because we see the damned in the same way
+we see the living who, at least in our own eyes, aren't necessarily living the
+most Christian life. The Catechism says the following:
+</p>
+
+<blockquote>
+	“We cannot be united with God unless we freely choose to love him. But we
+	cannot love God if we sin gravely against him, against our neighbor or
+	against ourselves. [...] This state of definitive self-exclusion from
+	communion with God and the blessed is called ‘hell.’” (CCC § 1033)
+</blockquote>
+
+<p>
+Whereas with the living we are incapable of judging our neighbor and the state
+of their soul, nor are we capable of knowing whether they shall eventually
+repent and be saved by God, with the damned we are actually certain of the
+sinful state of their soul and that they will never repent, for only these sorts
+of people can exist in Hell. Furthermore, we must remember that Hell is a “state
+of self-exclusion,” (ibid.) meaning that the damned person himself made the
+conscious decision to prefer sin and damnation over repentance and eternity in
+communion with the source and summit of all goodness; the <i>Summum Bonum</i>.
+Therefore the picture that begins to form is of many wicked people causing their
+own suffering, and then getting mad that they are suffering. It begins to be
+quite comical and remind us of the Psalm: “Let the wicked together fall into
+their own nets, while I escape.” (Ps. 141:10)
+</p>
+
+<p>
+To give an example of how this sort of thing can seem humorous, let's take the
+example of Wile E. Coyote and the Road Runner. In this cartoon the Coyote is
+constantly trying to trap the Road Runner with all sorts of schemes and plots,
+but the punch line of every joke is that the Road Runner seems wholly unaffected
+by the trap and the Coyote ends up falling into it himself. What's even more
+funny is when he gets frustrated and angry, when we can all see that he himself
+is the source of his own suffering, which he only exacerbates more by getting
+angry. To add another level of silliness, he doesn't get angry at himself (the
+source of his misery) but at the Road Runner whose only apparent crime was not
+falling into his trap. Similarly, we can say that this is how we will feel about
+the damned. They are angry at God for their punishment and blame Him for it,
+when it is their own hatred of God which brought this punishment upon them in
+the first place, and which continues to make them more miserable. What's more,
+even the Devil himself, to whom we often mistakenly attribute a power he does
+not have, we will see as nothing more than a pathetic and miserable being.
+</p>
+
+<p>
+It should be noted that the Church does not officially condemn anyone to Hell,
+nor should we assume anyone to be there besides Satan and the fallen angels. The
+Church's mission is not to condemn souls, but to provide the means of salvation
+to the world. Therefore it would not be wise to preemptively assume to be able
+to partake in this heavenly form of comedy, which I myself am only speculating
+as a possibility. Even in the most apparent case of someone most likely to be
+damned, Judas Iscariot, the Church does not officially condemn him for it is not
+her role.
+</p>
+
+<p>
+Ultimately this is all merely some theological speculation on my part, and I am
+perfectly willing to be wrong and change my mind. Send me an e-mail if you think
+that's the case (though if you make it super long I may take a long while to
+respond). I hope that at the very least this helped to reflect upon God and the
+economy of grace and mercy, to reflect on what we believe as Christians, and to
+strive for holiness in hope for the life to come, or at least in fear of the
+punishment that awaits those who do not repent and love God and neighbor. God
+bless you.
+</p>
+
+<?php
+require 'templates/blog-footer.php';
+?>
