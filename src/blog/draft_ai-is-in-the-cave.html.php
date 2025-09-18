a8181b3667195342944acc060499e198d38956b5
Nicolás Ortega Froysa <nicolas@ortegas.org>
2025-09-18 08:35:16 +0200

    Blog: Continue section on “Here Be Demons”

--- src/blog/draft_ai-is-in-the-cave.html.php
+++ src/blog/draft_ai-is-in-the-cave.html.php
@@ -163,21 +163,21 @@ consider what it <em>is</em> and what it is <em>for</em>.
 <p>
 For this I must refer to a video made by New Polity titled “Should Christians
 Use ChatGPT?”<sup><a href="#r2">[2]</a></sup> which is the only resource I have
-found as of yet which actually addresses the issue of the nature of LLMs &ndash;
-thought to be fair, I haven't done a lot of research on my own and I was already
-subscribed to them. I don't wish to go through the entire syllogism here,
-because that's what they made the video for, as well as their blog article “AI
-Chatbots Are Evil”<sup><a href="#r3">[3]</a></sup> (which probably spoils some of
-the answer from the title), but I do wish to briefly summarize the concluding
-statements taking for granted that we all accept basic virtue ethics. In short,
-we first start not with the LLMs and the chatbots, but with human conversation
-and its purpose, and note that, as mentioned in the article:
+found as of yet which actually addresses the issue of the nature of
+LLMs&mdash;thought to be fair, I haven't done a lot of research on my own and I
+was already subscribed to them. I don't wish to go through the entire syllogism
+here, because that's what they made the video for, as well as their blog article
+“AI Chatbots Are Evil”<sup><a href="#r3">[3]</a></sup> (which probably spoils
+some of the answer from the title), but I do wish to briefly summarize the
+concluding statements taking for granted that we all accept basic virtue ethics.
+In short, we first start not with the LLMs and the chatbots, but with human
+conversation and its purpose, and note that, as mentioned in the article:
 </p>
 
 <blockquote>
 “Conversation is for <em>communion</em>. The ability to speak and to listen, to
 discuss, to reveal our hidden, intellectual life by articulating ourselves in a
-public, common language with the hope of receiving a response&ndash;all this has
+public, common language with the hope of receiving a response&mdash;all this has
 as its natural correlate in another intelligence, one who receives our meaning,
 understands it (or misunderstands it), and has the power to respond in kind,
 revealing the hidden reality of his or her own subjectivity.”
@@ -185,24 +185,23 @@ revealing the hidden reality of his or her own subjectivity.”
 
 <p>
 If conversation is for communion with another intelligence, and LLMs solicit
-conversation by how they're programmed, yet LLMs &ndash; deceptively called AI
-for marketing purposes &ndash; are not actually intelligent, then to converse
-with a LLM chatbot will necessarily always frustrate the act of conversation. To
+conversation by how they're programmed, yet LLMs&mdash;deceptively called AI for
+marketing purposes&mdash;are not actually intelligent, then to converse with a
+LLM chatbot will necessarily always frustrate the act of conversation. To
 frustrate an act from achieving its end is immoral, therefore LLM chatbots are
 an immoral technology, and to converse with one is likewise an immoral act.
 </p>
 
 <p>
 This is a brief summary of the argument put forward by Marc Barnes of New
-Polity. For a fuller treatise on the matter either watch the
-video<sup><a href="#r2" >[2]</a></sup> or read the
-article.<sup><a href="#r3" >[3]</a></sup> For my purposes, however, I'd like to
-clarify a couple of things which I think need to be pointed out regarding this
-conclusion. First is to point out that claiming a technology is immoral by its
-nature is not something new or special to LLM chatbots (at least for Catholics);
-and second is to clearly delineate the limits of this argument, as it is very
-possible that one may wrongly assume this is an argument against all use of
-LLMs.
+Polity. For a fuller treatise on the matter either watch the video<sup><a href="#r2" >[2]</a></sup>
+or read the article.<sup><a href="#r3" >[3]</a></sup> For my purposes, however,
+I'd like to clarify a couple of things which I think need to be pointed out
+regarding this conclusion. First is to point out that claiming a technology is
+immoral by its nature is not something new or special to LLM chatbots (at least
+for Catholics); and second is to clearly delineate the limits of this argument,
+as it is very possible that one may wrongly assume this is an argument against
+all use of LLMs.
 </p>
 
 <p>
@@ -219,12 +218,12 @@ advocated in some videos<sup><a href="n3" >(3)</a></sup> and purposely sabotage
 a contraceptive you will use in order to attain some other end (he gives the
 example of acquiring seed from the marital act to use for artificial
 insemination), but I think we'd all recognize that this is not truly a use but a
-misuse of the technology: it's not what it was made for &ndash; hence the need
+misuse of the technology: it's not what it was made for&mdash;hence the need
 to intentionally sabotage it. The technology itself is evil because its natural
 purpose is evil and unnatural. Similarly we can also give the example of a
 nuclear bomb. The purpose for which this technology was made was to
 indiscriminately destroy and kill at a massive scale, necessarily killing
-millions of innocent civilians in the process &ndash; something which I hope
+millions of innocent civilians in the process&mdash;something which I hope
 doesn't have to be explained why it is extremely immoral. Again, surely you
 could instead use a nuclear bomb to destroy or deflect a giant asteroid headed
 to Earth, but this is not what the technology was made for. Similarly with the
@@ -245,11 +244,11 @@ related to language at all (even though it's in the name), but rather it's about
 predicting based on prior tokens what the next token is likely to be. As such
 the argument doesn't touch upon things such as how LLMs are used for
 autocompletion of the next few words when you write your e-mails, or of the next
-few lines of redundant code if you're a programmer &ndash; though there are
-legal concerns over violation of intellectual property rights. It also doesn't
-apply to cases such as archaeologists who have used LLMs to “fill in missing
-words from ancient Roman inscriptions carved on monuments and everyday objects,
-as well as dating and placing them geographically,”<sup><a href="#r4" >[4]</a></sup>
+few lines of redundant code if you're a programmer&mdash;though there are legal
+concerns over violation of intellectual property rights. It also doesn't apply
+to cases such as archaeologists who have used LLMs to “fill in missing words
+from ancient Roman inscriptions carved on monuments and everyday objects, as
+well as dating and placing them geographically,”<sup><a href="#r4">[4]</a></sup>
 nor cases such as the Alphafold tool which has been used to predict with amazing
 accuracy the 3D structure of proteins,<sup><a href="#r5" >[5]</a></sup>
 something which would have taken lots of expensive and tedious manual labor to
@@ -264,6 +263,59 @@ actually use the LLM as a tool which assists man in his endeavors.
 </p>
 
 <h2>Here Be Demons</h2>
+
+<p>
+This is probably where I will lose the entirety of the secularist (or
+quasi-secularist) section of my readers, since we delve much more into the
+spiritual, but I think it is worth addressing due to the grave danger it poses
+to the souls of men. We must talk about the potential for demonic influence in
+these LLM chatbots.
+</p>
+
+<p>
+When speaking of this there are typically two reactions: on the one hand you
+have those who, incapable of understanding how a machine could generate text
+that feels human, claim it all to be demonic and powered by the Devil himself;
+on the other you have those who completely reject the idea because they frankly
+find the notion of the demonic, particularly in computing, to be absurd. I will
+take neither of these positions and instead choose a third middle route, which I
+think is more prudent and theologically sound.
+</p>
+
+<p>
+We must not assume that simply because we do not understand the technology that
+it is something demonic. Furthermore, there is an actual logical way of
+understanding how LLMs work which can easily be found in the publicly available
+source-code for some of these models.<sup><a href="#r6" >[6]</a></sup> However,
+that is not to say the demonic cannot influence a computer and a computer
+algorithm, particularly one that, as already stated above, is made for an evil
+purpose&mdash;to solicit conversation from intelligent beings in spite of not
+being intelligent itself and thus not being able to actually hold a
+conversation. This is made by an even more probable by the fact that these LLM
+chatbots are being turned into literal idols to which people entrust their
+livelihoods. This is not an exaggeration; an increasing number of people are
+beginning to use these chatbots as a means of therapy<sup><a href="#r7" >[7]</a></sup>
+or making other important decisions in their lives. What's more, arguably
+because of the somewhat mystical omniscience attributed often to these LLM
+chatbots, people will often put more faith in the answer given by the chatbot
+than a human being or their own personal research&mdash;we've all had the
+experience of having a conversation with someone and when a dispute arises they
+take out their phone ask the chatbot and trust its judgment as if it were final.
+This turns these chatbots into idols, not just in the very loose sense we're
+used to hearing from our priests and catechists where it's idolatry for me to
+like my game console too much (an example I've heard used quite a bit for some
+reason), but in the very real sense that as a society we are assigning divine
+attributes (sometimes implicitly, sometimes explicitly) to, not just a creature,
+but a creature of our own making: just as the ancient heathens would assign
+divine attributes to the statues they would make of matter that in some way
+resembled a human being or another creature.
+</p>
+
+<p>
+When this sort of idolatry happens&mdash;already a sin against the First
+Commandment&mdash;it is no surprise that just as with the idols of old, demons
+can also use the new idols to manipulate men to stray from God.
+</p>
 
 <h2>Resources</h2>
 <h3>Notes</h3>
@@ -292,6 +344,8 @@ actually use the LLM as a tool which assists man in his endeavors.
 	<li id="r3" ><a href="https://newpolity.com/blog/ai-chatbots" target="_blank" >AI Chatbots Are Evil — NEWPOLITY</a></li>
 	<li id="r4" ><a href="https://www.bbc.com/news/articles/c04dwqr5lkvo" target="_blank" >Gaps in what we know about ancient Romans could be filled by AI - BBC</a></li>
 	<li id="r5" ><a href="https://alphafold.com/" target="_blank" >AlphaFold Protein Structure Database</a></li>
+	<li id="r6" ><a href="https://blog.n8n.io/open-source-llm/" >The 11 best open-source LLMs for 2025 – n8n Blog</a></li>
+	<li id="r7" ><a href="https://health.usnews.com/wellness/mind/articles/should-you-use-artificial-intelligence-ai-as-your-therapist" target="_blank" >Should You Use AI as Your Therapist? |U.S. News</a></li>
 </ol>
 
 <?php
